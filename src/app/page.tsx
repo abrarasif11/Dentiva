78d4c6d5c9466f349b139408267ca2d3991b604b
abrarasif11 <faasif929@gmail.com>
2026-02-09 18:40:43 +0600

    clerk setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
 import { Button } from "@/components/ui/button";
+import {
+  SignedIn,
+  SignedOut,
+  SignOutButton,
+  SignUpButton,
+} from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click Me</Button>
+      <h1>Home Page</h1>
+      <SignedOut>
+        <SignUpButton mode="modal">Sign Up</SignUpButton>
+      </SignedOut>
+      <SignedIn>
+        <SignOutButton>Log Out</SignOutButton>
+      </SignedIn>
     </div>
   );
 }

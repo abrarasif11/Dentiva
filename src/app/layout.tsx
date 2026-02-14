5f90344147f5892dcc82aa3852e45d515ce46f27
abrarasif11 <faasif929@gmail.com>
2026-02-14 14:20:08 +0600

    change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import UserSync from "@/components/UserSync";
+
 import TansStackProvider from "@/components/providers/TanStackProvider";
 import { Toaster } from "sonner";
 
@@ -20,6 +20,9 @@ export const metadata: Metadata = {
   title: "Dentiva - AI Powered Dental Assistant",
   description:
     "Get instant dental advice through voice calls with our AI assistant. Available 24/7.",
+  icons: {
+    icon: "/public/logo.png",
+  },
 };
 
 export default function RootLayout({
